983176d0e5847281644d076948574345fe29c72c
johnlauren <aplusview@gmail.com>
2023-11-30 23:16:49 -0500

    add media files

--- sentiment_analysis/secretvars.py
+++ sentiment_analysis/secretvars.py
@@ -3,15 +3,20 @@
 
 """
 
-'''
-    use your own key/endpoint
+AZURE_COG_KEY = "2154bea2f38c48f4a0988131e1657990" #os.environ.get('AZURE_COG_KEY')
 
-    create account: Azure AI Services (multi-service)
+AZURE_COG_ENDPOINT = "https://vci-cog-svc.cognitiveservices.azure.com/" # os.environ.get('AZURE_COG_ENDPOINT')
 
-    paste in your key and endpoint below
 
-'''
+# '''
+#     use your own key/endpoint
 
-AZURE_COG_KEY = "<Paste_your_key>"
+#     create account: Azure AI Services (multi-service)
 
-AZURE_COG_ENDPOINT = "<paste_your_endpoint"
+#     paste in your key and endpoint below
+
+# '''
+
+# AZURE_COG_KEY = "<Paste_your_key>"
+
+# AZURE_COG_ENDPOINT = "<paste_your_endpoint"
