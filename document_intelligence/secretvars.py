89edb83db4ed6600dc08db1ead7224fad9535f18
h5sharp@hotmail.com <aplusview@gmail.com>
2025-08-12 18:05:10 -0400

    add var files

---
+++ document_intelligence/secretvars.py
@@ -0,0 +1,18 @@
+"""
+    keys and secret content
+
+"""
+
+
+# """
+#     use your own key/endpoint
+
+#     create account: Azure AI Document Intelligence
+
+#     paste in your key and endpoint below
+
+# """
+
+AZURE_DOCINTEL_KEY = "<Paste_your_key>"
+
+AZURE_DOCINTEL_ENDPOINT = "<paste_your_endpoint>"
---
+++ sentiment_analysis/secretvars.py
@@ -0,0 +1,18 @@
+"""
+    keys and secret content
+
+"""
+
+
+# '''
+#     use your own key/endpoint
+
+#     create account: Azure AI Services (multi-service)
+
+#     paste in your key and endpoint below
+
+# '''
+
+AZURE_COG_KEY = "<Paste_your_key>"
+
+AZURE_COG_ENDPOINT = "<paste_your_endpoint"
