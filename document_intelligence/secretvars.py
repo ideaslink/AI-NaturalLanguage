6cb9fa434f25465e6af8d84a6b2c2e9c80a9ea9f
h5sharp@hotmail.com <aplusview@gmail.com>
2025-08-12 17:37:11 -0400

    update .gitignore

--- document_intelligence/secretvars.py
+++ document_intelligence/secretvars.py
@@ -3,6 +3,10 @@
 
 """
 
+AZURE_DOCINTEL_KEY = "c07f5c3a4ce34ee4ab72f1d57a6a0bab"
+
+AZURE_DOCINTEL_ENDPOINT = "https://docinteli-hsharp.cognitiveservices.azure.com/"
+
 # """
 #     use your own key/endpoint
 
@@ -12,6 +16,6 @@
 
 # """
 
-AZURE_DOCINTEL_KEY = "<Paste_your_key>"
+# AZURE_DOCINTEL_KEY = "<Paste_your_key>"
 
-AZURE_DOCINTEL_ENDPOINT = "<paste_your_endpoint>"
+# AZURE_DOCINTEL_ENDPOINT = "<paste_your_endpoint>"
